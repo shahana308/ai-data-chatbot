bc00cbdf012f7c87b80584f46ea0230b8555da0d
Shahana Salam <shahana.salam@cafu.com>
2025-03-09 22:27:38 +0400

    initial commit

--- backend/routes/ai_chatbot.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request, jsonify
-
-ai_chatbot_blueprint = Blueprint('ai_chatbot', __name__)
-
-@ai_chatbot_blueprint.route('/ask', methods=['POST'])
-def ask_question():
-    # Process AI query and return response
-    return jsonify({"response": "AI response here"}) 
