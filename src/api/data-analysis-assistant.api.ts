5b23e6b9f0019bb9ab1dcad42e37c09409c295e1
Shahana Salam <shahana.salam@cafu.com>
2025-03-13 13:21:14 +0400

    feat: add chat types

--- src/api/data-analysis-assistant.api.ts
+++ src/api/data-analysis-assistant.api.ts
@@ -1,12 +1,13 @@
+import { DataAnalysisResponse } from "@/types/DataAnalysis";
 import axios from "axios";
 
 const API_URL = "https://openrouter.ai/api/v1/chat/completions";
 const API_KEY =
-  "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";
+  "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";
 
 export const fetchDataAnalysis = async (content: string) => {
   try {
-    const response = await axios.post(
+    const response = await axios.post<DataAnalysisResponse>(
       API_URL,
       {
         model: "deepseek/deepseek-r1-zero:free",
---
+++ src/types/DataAnalysis.ts
@@ -0,0 +1,7 @@
+export interface DataAnalysisResponse {
+  choices: {
+    message: {
+      content: string;
+    };
+  }[];
+}
