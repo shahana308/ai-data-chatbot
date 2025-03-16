af2ceebb63b760a50b711a2aab1f0bdb55efdc6a
Shahana Salam <shahana.salam@cafu.com>
2025-03-16 12:47:28 +0400

    style: update layout for improved styling and responsiveness

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div className="w-1/5 bg-gray-800 text-white h-screen">Sidebar</div>;
+  return <div className="w-1/5 text-white h-screen">Sidebar</div>;
 };
 
 export default Sidebar;
