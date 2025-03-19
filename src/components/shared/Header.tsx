76804525bc6c5684b95a915946d910dc616326a4
Shahana Salam <shahana.salam@cafu.com>
2025-03-19 14:25:55 +0400

    refactor: add divider instead of border

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -4,21 +4,21 @@ const { Title } = Typography;
 
 interface HeaderProps {
   level?: 1 | 2 | 3 | 4 | 5;
+  title: string | React.ReactNode;
   className?: string;
   titleClass?: string;
 }
 
 const Header: React.FC<HeaderProps> = ({
   level = 4,
+  title,
   className = "",
   titleClass = "#000",
 }) => {
   return (
-    <div
-      className={`sticky top-0 z-10 p-2 bg-white border-b border-gray-200 ${className}`}
-    >
+    <div className={`sticky top-0 z-10 p-2 bg-white !mb-0 ${className}`}>
       <Title level={level} className={titleClass}>
-        Chat History
+        {title}
       </Title>
     </div>
   );
